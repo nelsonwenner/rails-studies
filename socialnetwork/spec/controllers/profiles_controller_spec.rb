dfb7464ed7909ed8bceebc00771070ceeb4c5187
wenner <nelsondiasdemedeiros@gmail.com>
2020-12-09 10:51:56 -0300

    feat: add factory profile and controller

---
+++ socialnetwork/spec/controllers/profiles_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe ProfileController, :type => :controller do
+  
+
+end
--- socialnetwork/spec/factories/profiles.rb
+++ socialnetwork/spec/factories/profiles.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :profile do
-    
+    name {Faker::Name.name}
+    email {Faker::Internet.email}
+    address {
+      street: {Faker::Address.street_name},
+      suite: {Faker::Address.building_number},
+      city: {Faker::Address.city},
+      zipcode: {Faker::Address.zip_code} 
+    }
   end
+
 end
