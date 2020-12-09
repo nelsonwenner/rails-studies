d83c77c4f10b9179674d62b1a5cee067cbdbef80
wenner <nelsondiasdemedeiros@gmail.com>
2020-12-09 10:08:13 -0300

    test: add tests validation

--- socialnetwork/app/models/address.rb
+++ socialnetwork/app/models/address.rb
@@ -1,2 +1,5 @@
 class Address < ApplicationRecord
+  belongs_to :profile
+  
+  validates_presence_of :street, :suite, :city, :zipcode, on: :create
 end
---
+++ socialnetwork/db/schema.rb
@@ -0,0 +1,34 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_12_08_215412) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "street"
+    t.integer "suite"
+    t.string "city"
+    t.string "zipcode"
+    t.integer "profile_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["profile_id"], name: "index_addresses_on_profile_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "addresses", "profiles"
+end
