6640cdd4936a9b52ad57601c5c5163dc2ed7e0cc
wenner <nelsondiasdemedeiros@gmail.com>
2020-12-22 21:38:05 -0300

    test: add test in models

--- socialnetwork/spec/models/friendship_spec.rb
+++ socialnetwork/spec/models/friendship_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Friendship, type: :model do
- 
+RSpec.describe Friendship, :type => :model do
+  describe 'Associations' do
+    it { should belong_to(:profile) }
+    it { should belong_to(:friend) }
+  end
 end
--- socialnetwork/spec/models/invitation_spec.rb
+++ socialnetwork/spec/models/invitation_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Invitation, :type => :model do
-
+  describe 'Associations' do
+    it { should belong_to(:requester) }
+    it { should belong_to(:guest) }
+  end
 end
