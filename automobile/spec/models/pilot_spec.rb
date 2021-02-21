d0271b2cd00e94416d1d8f31c0033813c558ee3f
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-21 12:02:21 -0300

    test: ensure be able to validate uniqueness of name

--- automobile/spec/models/pilot_spec.rb
+++ automobile/spec/models/pilot_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Pilot, type: :model do
   describe 'Validations' do
     it { should validate_presence_of(:name).on(:create) }
+    it { should validate_uniqueness_of(:name) }
   end
 end
