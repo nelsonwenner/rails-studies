19b1f42598f60348764ecc4f37b16dd60a276ccd
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-21 13:49:38 -0300

    feat: add validation presence fields

--- automobile/app/models/race.rb
+++ automobile/app/models/race.rb
@@ -1,2 +1,4 @@
 class Race < ApplicationRecord
+  validates_presence_of :date, :total_laps, :total_time, :best_lap, 
+                        :time_best_lap, :starting_grid, :average_velocity, on: :create
 end
