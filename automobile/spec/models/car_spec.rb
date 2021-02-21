a7ceeb8871933b1371118cef9a15588704f55f45
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-21 12:35:30 -0300

    test: ensure be able to validate uniqueness of number

--- automobile/spec/models/car_spec.rb
+++ automobile/spec/models/car_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Car, type: :model do
   describe 'Validations' do
     it { should validate_presence_of(:number).on(:create) }
+    it { should validate_uniqueness_of(:number) }
   end
 end
