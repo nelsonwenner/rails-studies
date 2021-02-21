37a0b1b94d08955b8bc50a5a055626a4c4bec638
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-21 12:01:36 -0300

    test: ensure be able to validate presence field name

--- automobile/spec/models/pilot_spec.rb
+++ automobile/spec/models/pilot_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Pilot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it { should validate_presence_of(:name).on(:create) }
+  end
 end
