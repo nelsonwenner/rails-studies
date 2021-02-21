5ad5fc22760a96075e97c65ec68b1ca47c33fc69
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-21 12:04:25 -0300

    test: ensure be able to validate presence field year

--- automobile/spec/models/tournament_spec.rb
+++ automobile/spec/models/tournament_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Tournament, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it { should validate_presence_of(:year).on(:create) }
+  end
 end
