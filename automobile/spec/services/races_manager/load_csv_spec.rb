57f0431d510b722d866610d301afe2de2d46c11f
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-21 00:38:29 -0300

    test: ensure be able to validate a date valid

--- automobile/spec/services/races_manager/load_csv_spec.rb
+++ automobile/spec/services/races_manager/load_csv_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe RacesManager::LoadCsv do
-  describe "#format_date" do
-    let(:loadcsv) { RacesManager::LoadCsv.new("test") }
+  let(:loadcsv) { RacesManager::LoadCsv.new("test") }
 
+  describe "#format_date" do
     it "Should be able to format the date" do
       expect(loadcsv.format_date("race_2019_01_31.csv")).to eq("2019/01/31")
+    end
+  end
+
+  describe "#is_valid_date" do
+    it "Should be able to validate a date valid" do
+      expect(loadcsv.is_valid_date("2019/01/31")).to eq("2019-01-31T00:00:00+00:00")
     end
   end
 end
