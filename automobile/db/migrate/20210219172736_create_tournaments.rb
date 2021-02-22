fad9a66c7816bc03a28d89ff2740e3149db4669a
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-22 11:27:16 -0300

    refactor: changed field type year to interger in migrate

--- automobile/db/migrate/20210219172736_create_tournaments.rb
+++ automobile/db/migrate/20210219172736_create_tournaments.rb
@@ -1,7 +1,7 @@
 class CreateTournaments < ActiveRecord::Migration[6.0]
   def change
     create_table :tournaments do |t|
-      t.string :year
+      t.integer :year
 
       t.timestamps
     end
