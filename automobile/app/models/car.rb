bb9ddcf543c483601c7e6d0fdce0fd2cb352ab74
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-22 11:24:43 -0300

    refactor: add message in validates uniqueness

--- automobile/app/models/car.rb
+++ automobile/app/models/car.rb
@@ -1,4 +1,4 @@
 class Car < ApplicationRecord
   validates_presence_of :number, on: :create
-  validates_uniqueness_of :number
+  validates_uniqueness_of :number, message: "Already exists a car with this number."
 end
--- automobile/app/models/pilot.rb
+++ automobile/app/models/pilot.rb
@@ -1,4 +1,4 @@
 class Pilot < ApplicationRecord
   validates_presence_of :name, on: :create
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, message: "Already exists a pilot with this name."
 end
