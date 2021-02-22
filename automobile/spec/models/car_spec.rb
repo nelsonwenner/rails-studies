8cdc49e0da7577b2a1f4d228e470301610eea383
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-22 12:13:43 -0300

    refactor: refactored tests

--- automobile/spec/models/car_spec.rb
+++ automobile/spec/models/car_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Car, type: :model do
   describe 'Validations' do
     it { should validate_presence_of(:number).on(:create) }
-    it { should validate_uniqueness_of(:number) }
+    it { should validate_uniqueness_of(:number).with_message("Already exists a car with this number.")  }
   end
 end
--- automobile/spec/models/pilot_spec.rb
+++ automobile/spec/models/pilot_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Pilot, type: :model do
   describe 'Validations' do
     it { should validate_presence_of(:name).on(:create) }
-    it { should validate_uniqueness_of(:name) }
+    it { should validate_uniqueness_of(:name).with_message("Already exists a pilot with this name.")  }
   end
 end
