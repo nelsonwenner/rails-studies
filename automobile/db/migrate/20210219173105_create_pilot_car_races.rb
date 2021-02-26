d223cc560599702172a5a3d605fa71b74fa1affd
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-26 15:06:18 -0300

    refactor: renamed name

--- automobile/db/migrate/20210219173105_create_pilot_car_races.rb
+++ automobile/db/migrate/20210219173105_create_pilot_car_races.rb
@@ -1,6 +1,6 @@
-class CreatePilotsCarsRaces < ActiveRecord::Migration[6.0]
+class CreatePilotCarRaces < ActiveRecord::Migration[6.0]
   def change
-    create_table :pilots_cars_races do |t|
+    create_table :pilot_car_races do |t|
       t.references :race, null: false, foreign_key: { to_table: :races }
       t.references :pilot, null: false, foreign_key: { to_table: :pilots }
       t.references :car, null: false, foreign_key: { to_table: :cars }
