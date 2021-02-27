5439f1d9c70d6305486cd466dede1b296e5c0b63
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-27 19:04:16 -0300

    test: ensure not be able to process CSV date invalid

--- automobile/spec/controllers/races_controller_spec.rb
+++ automobile/spec/controllers/races_controller_spec.rb
@@ -2,13 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::RacesController, type: :controller do
   describe "POST #create" do
-    let(:csv) { Rack::Test::UploadedFile.new(Rails.root.join(
+    let(:csv_valid) { Rack::Test::UploadedFile.new(Rails.root.join(
     'spec', 'fixtures', 'files', 'race_2019_01_31.csv')) }
+    let(:csv_invalid_date) { Rack::Test::UploadedFile.new(Rails.root.join(
+    'spec', 'fixtures', 'files', 'race_2019_02_31.csv')) }
+    let(:csv_invalid_format) { Rack::Test::UploadedFile.new(Rails.root.join(
+    'spec', 'fixtures', 'files', 'race_name_invalid_01_02.csv')) }
 
     it 'Should be able to process CSV and return status successfully' do
-      response = post :create, params: { race_file: csv }
+      response = post :create, params: { race_file: csv_valid }
       expect(eval(response.body)[:message]).to eq("successfully")
       expect(response.status).to equal(200)
+    end
+
+    it 'Should not be able to process CSV date invalid' do
+      response = post :create, params: { race_file: csv_invalid_date }
+      expect(eval(response.body)[:errors]).to eq("Date invalid")
+      expect(response.status).to equal(422)
     end
   end
 end
