51d04cea8feb70dbc12e80ff1d3bb67a0bd6f5bd
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-27 23:08:52 -0300

    test: ensure be able to constraint pair of unique value

--- automobile/spec/models/pilot_car_race_spec.rb
+++ automobile/spec/models/pilot_car_race_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 RSpec.describe PilotCarRace, type: :model do
   describe 'Associations' do
-    it { should belong_to(:pilot) }
-    it { should belong_to(:car) }
-    it { should belong_to(:race) }
+    it { should belong_to(:pilot).class_name('Pilot') }
+    it { should belong_to(:car).class_name('Car') }
+    it { should belong_to(:race).class_name('Race') }
+  end
+
+  describe 'Uniqueness' do
+    subject { create(:pilot_car_race) }
+    it { should validate_uniqueness_of(:race_id).scoped_to(:pilot_id, :car_id).with_message("There is already this pilot and car in this race. Constraint pair of unique value.") }
   end
 end
