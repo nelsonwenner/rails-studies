dc28061824875b40f5fecbb27155b96cff370e39
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-28 15:50:14 -0300

    feat: verify quantity model PilotCarRace, only 14 pilots/cars are allowed per race

--- automobile/app/models/pilot_car_race.rb
+++ automobile/app/models/pilot_car_race.rb
@@ -1,17 +1,17 @@
 class PilotCarRace < ApplicationRecord
-  #after_initialize :init
+  after_initialize :verify_quantity_race_cars_pilots
 
   belongs_to :pilot, class_name: 'Pilot'
   belongs_to :car, class_name: 'Car'
   belongs_to :race, class_name: 'Race'
-
+ 
   validates_uniqueness_of :race_id, :scope => [:pilot_id, :car_id], message: "There is already this pilot and car in this race. Constraint pair of unique value."
 
   private
 
-  #def init
-  #  if self.where(tournament_id: self.tournament_id).count >= 14
-  #    self.errors.add("Only 14 drivers/cars are allowed per race.")
-  #  end 
-  #end
+  def verify_quantity_race_cars_pilots
+    if PilotCarRace.where(race_id: self.race_id).count >= 14
+      raise Exception.new "Only 14 pilots/cars are allowed per race."
+    end 
+  end
 end
