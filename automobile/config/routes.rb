2d6b91e88c476ce908d87f0375548caebaad19bc
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-01 22:15:00 -0300

    chore: add a new route

--- automobile/config/routes.rb
+++ automobile/config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :races, only: [:create]
-      resources :tournaments, param: :year, only: [:index, :show]
+      
+      resources :tournaments, param: :year, only: [:index, :show] do
+        resources :races, only: [:index]
+      end
     end
   end
 end
