335cd456d596e822495b3ffb0621d8886e8399f1
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-01 22:26:24 -0300

    test: ensure be able to if relate with many PilotCarRace

--- automobile/spec/models/race_spec.rb
+++ automobile/spec/models/race_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Race, type: :model do
   describe 'Associations' do
     it { should belong_to(:tournament) }
+    it { should have_many(:pilot_car_races).class_name('PilotCarRace') }
   end
 
   describe 'Validations' do
