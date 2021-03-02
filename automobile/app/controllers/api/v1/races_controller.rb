6ebf2cebf57ffff8ccbb16f24f041c2f1a695353
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-01 22:16:56 -0300

    feat: add index

--- automobile/app/controllers/api/v1/races_controller.rb
+++ automobile/app/controllers/api/v1/races_controller.rb
@@ -11,7 +11,18 @@ module Api
         end
       end
 
+      def index
+        render status: 200, json: races, each_serializer: RaceSerializer
+      end
+
       private
+
+      def races
+        tournament = Tournament.where(
+          year: params[:tournament_year]
+        ).first! or not_found
+        @races ||= Race.where(tournament_id: tournament.id)
+      end
 
       def race_params
         params.require(:race_file)
