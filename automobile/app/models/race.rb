5b7bf51f605d010049ca443771345d23ab83ef7b
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-01 22:15:23 -0300

    feat: add a new relations

--- automobile/app/models/race.rb
+++ automobile/app/models/race.rb
@@ -1,4 +1,5 @@
 class Race < ApplicationRecord
+  has_many :pilot_car_races, class_name: 'PilotCarRace'
   belongs_to :tournament, class_name: 'Tournament'
 
   validates_presence_of :date, on: :create
